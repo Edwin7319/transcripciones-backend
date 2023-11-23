b6cd53bfe174bdb4d6bcd1a7547c8b478ddcc746
Edwin <edwin.paul.73@gmail.com>
2023-11-23 00:26:11 -0500

    feature: configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,40 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
+    ecmaVersion: 2021,
     sourceType: 'module',
+    project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint', 'import'],
   extends: [
     'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
   ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
+    'no-magic-numbers': 'off',
+    'no-underscore-dangle': 'off',
+    'space-before-function-paren': 'off',
+    'indent': ['error', 2],
+    'max-len': ['error', {'code': 120}],
+    'camelcase': 'error',
+    'no-duplicate-imports': 'error',
+    'no-dupe-class-members': 'error',
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        'alphabetize': {
+          'order': 'asc',
+          'caseInsensitive': true
+        },
+        'groups': ['external', 'internal', 'parent', 'sibling', 'index'],
+        'newlines-between': 'always'
+      }
+    ]
   },
 };
