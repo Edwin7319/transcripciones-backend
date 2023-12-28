1d88472a210a8f6f02a6bf6a78fd1ad726ca2a14
Edwin <edwin.paul.73@gmail.com>
2023-12-27 22:04:15 -0500

    feat: update feature command

--- src/modules/commands/command.service.ts
+++ src/modules/commands/command.service.ts
@@ -6,12 +6,19 @@ const { exec } = require('node:child_process');
 @Injectable()
 export class CommandService {
   async executeCommand(
+    audioName: string,
+    fileName: string,
     parameters = '',
   ): Promise<{ message: string; output: string }> {
     return new Promise((res, rej) => {
-      const commandFromEnvironmentVariable = process.env.MLABS_COMMAND
-        ? process.env.MLABS_COMMAND
-        : 'ls ./';
+      let commandFromEnvironmentVariable = 'ls ./';
+      const mlabsCommand = process.env.MLABS_COMMAND;
+      if (mlabsCommand) {
+        commandFromEnvironmentVariable = mlabsCommand
+          .replace(':audioName', audioName)
+          .replace(':fileName', fileName);
+      }
+      console.log({ commandFromEnvironmentVariable });
       exec(
         commandFromEnvironmentVariable + parameters,
         (err, output: string) => {
