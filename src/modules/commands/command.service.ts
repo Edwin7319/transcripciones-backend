671cc7072cea8834d565c1e0366c0a20cac693bb
Adrian Eguez <adrian.eguez@onecowstanding.com>
2023-12-28 10:06:40 -0500

    feat: create spawn process

--- src/modules/commands/command.service.ts
+++ src/modules/commands/command.service.ts
@@ -1,17 +1,48 @@
 import { Injectable } from '@nestjs/common';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { exec } = require('node:child_process');
+const { exec, spawn } = require('node:child_process');
 
 @Injectable()
 export class CommandService {
   async executeCommand(
     parameters?: string,
+    enableSpawn = true,
   ): Promise<{ message: string; output: string }> {
     return new Promise((res, rej) => {
       const commandFromEnvironmentVariable = process.env.MLABS_COMMAND
         ? process.env.MLABS_COMMAND
         : 'ls ./';
+      if (enableSpawn) {
+        try {
+          let output = '';
+          const childSpawn = spawn(
+            commandFromEnvironmentVariable + parameters,
+            [],
+            {
+              detached: true,
+              shell: true,
+            },
+          );
+          childSpawn.stdout.on('data', (data) => {
+            output = output + data;
+          });
+          childSpawn.on('exit', (code) => {
+            if (code === 0) {
+              res({ message: 'Command executed succesfully', output });
+            }
+            rej({
+              error: `Error code ${code}`,
+              message: 'Could not execute commmand',
+            });
+          });
+        } catch (error) {
+          rej({
+            error: error,
+            message: 'Could not execute commmand',
+          });
+        }
+      }
       exec(
         commandFromEnvironmentVariable + parameters,
         (err, output: string) => {
