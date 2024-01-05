4d96a18159a503b24f959684508306ba4cd82b39
Edwin <edwin.paul.73@gmail.com>
2024-01-05 01:05:26 -0500

    feat: cambios auditoria service

--- src/modules/log/log.controller.ts
+++ src/modules/log/log.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
+
+import { PaginationDto } from '../../shared/pagination.dto';
+
+import { ELogSchema, LogDocument } from './log.schema';
+import { LogService } from './log.service';
 
 @Controller('log')
-export class LogController {}
+export class LogController {
+  constructor(private readonly _logService: LogService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Get(':schemaType')
+  getBySchema(
+    @Param('schemaType') schemaType: ELogSchema,
+  ): Promise<PaginationDto<LogDocument>> {
+    return this._logService.getBySchema(schemaType);
+  }
+}
